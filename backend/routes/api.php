ea52c7d4f6fcf3651034605ef008dd9e11ca96d5
Josias <kologojosias@gmail.com>
2025-08-22 02:02:58 +0100

    create api route base structure

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => '/api/v1'], function() {
+    Route::get('/health', '');
+    Route::get('/sources', '');
+    Route::get('categories', '');
+    Route::get('/authors', '');
+
+    Route::group(['prefix'=> '/articles'], function() {
+        Route::get('/', '');
+        Route::get('/{id}', '');
+    });
+
+    Route::group(['prefix'=> '/auth'], function() {
+        Route::post('/login', '');
+        Route::post('/register', '');
+        Route::post('/logout', '')->middleware('auth:sanctum');
+    });
+
+    Route::group(['prefix'=> '/me'], function() {
+        Route::get('/', '');
+        Route::get('/feed', '');
+        Route::get('/preferences', '');
+    })->middleware('auth:sanctum');
+});
